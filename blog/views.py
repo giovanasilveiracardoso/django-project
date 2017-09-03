37182de6ce9294a29a6f2c68d49b61b2f8511689
Giovana Cardoso Berti <giovanasilveiracardoso@gmail.com>
2017-09-02 23:06:51 -0300

    Criação de template, criação de página que utilizam o template e criação de css

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Post
@@ -6,4 +6,10 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    print(posts[0].pk)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/post_detail.html', {'post': post})
